6fd4bff429d2fa675ce473c52faab89fd49881be
NPC-22 <chouhangaj0722@gmail.com>
2024-12-24 01:40:19 +0530

    DecemberUpdate

---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:onboarding_app/routes/app_routes.dart';
+
+import 'animations/storage_service.dart';
+import 'dependency_injection.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarIconBrightness: Brightness.dark,
+    systemNavigationBarColor: Colors.white,
+    statusBarColor: Colors.transparent.withOpacity(0.0),
+  ));
+  await Get.putAsync(() async => StorageService());
+  await GetStorage.init();
+  DependencyInjection.init();
+  Get.put(GetStorage());
+  var initialRoute = await AppPages.initialRoute;
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(
+      DevicePreview(
+        enabled: true, // Set to false in production
+        builder: (context) => MyApp(initialRoute), // Wrap your app
+      ),
+    );
+  });
+}
+
+// Configure EasyLoading
+void configLoading() {
+  EasyLoading.instance
+    ..indicatorType = EasyLoadingIndicatorType.circle
+    ..loadingStyle = EasyLoadingStyle.dark
+    ..indicatorSize = 45.0
+    ..radius = 10.0
+    ..progressColor = Colors.yellow
+    ..backgroundColor = Colors.green
+    ..indicatorColor = Colors.yellow
+    ..textColor = Colors.yellow
+    ..maskColor = Colors.blue.withOpacity(0.5)
+    ..userInteractions = true
+    ..dismissOnTap = false;
+}
+
+class MyApp extends StatelessWidget {
+  final String initialRoute;
+
+  MyApp(this.initialRoute);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Onboarding App',
+      initialRoute: initialRoute,
+      getPages: AppPages.routes,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      builder: (context, child) {
+        return EasyLoading.init()(context, DevicePreview.appBuilder(context, child));
+      },
+      locale: DevicePreview.locale(context), // Add locale from DevicePreview
+    );
+  }
+}
+
+//
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_easyloading/flutter_easyloading.dart';
+// import 'package:get/get.dart';
+// import 'package:get_storage/get_storage.dart';
+// import 'package:onboarding_app/routes/app_routes.dart';
+//
+// import 'animations/storage_service.dart';
+// import 'dependency_injection.dart';
+//
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+//     statusBarIconBrightness: Brightness.dark,
+//     systemNavigationBarColor: Colors.white,
+//     statusBarColor: Colors.transparent.withOpacity(0.0), // status bar color
+//   ));
+//   await Get.putAsync(() async => StorageService());
+//   await GetStorage.init();
+//   DependencyInjection.init();
+//   Get.put(GetStorage());
+//   var initialRoute = await AppPages.initialRoute;
+//
+//   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+//       .then((_) {
+//     runApp(MyApp(initialRoute));
+//   });
+// }
+//
+// // Configure EasyLoading
+// void configLoading() {
+//   EasyLoading.instance
+//     ..indicatorType = EasyLoadingIndicatorType.circle
+//     ..loadingStyle = EasyLoadingStyle.dark
+//     ..indicatorSize = 45.0
+//     ..radius = 10.0
+//     ..progressColor = Colors.yellow
+//     ..backgroundColor = Colors.green
+//     ..indicatorColor = Colors.yellow
+//     ..textColor = Colors.yellow
+//     ..maskColor = Colors.blue.withOpacity(0.5)
+//     ..userInteractions = true
+//     ..dismissOnTap = false;
+// }
+//
+// class MyApp extends StatelessWidget {
+//   final String initialRoute;
+//
+//   MyApp(this.initialRoute);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return GetMaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: 'Onboarding App',
+//       initialRoute: initialRoute,
+//       getPages: AppPages.routes,
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       builder: EasyLoading.init(), // Initialize EasyLoading
+//     );
+//   }
+// }
